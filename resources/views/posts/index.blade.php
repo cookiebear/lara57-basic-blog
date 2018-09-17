305c4742510a914cb0999597b4f7e5816874635a
cookiebear <cookiebear@cookiebearui-MacBook-Pro.local>
2018-09-17 18:21:20 +0900

    [Eloquent & 댓글 추가] 1. Comment shema 추가 및 관계 추가 2. 댓글 화면 추가

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.master')
 
 @section('content')
-<div class="col-md-8 blog-main">
     @foreach ($posts as $post)
         @include('posts.article')
     @endforeach
@@ -11,7 +10,5 @@
     <a class="btn btn-outline-primary" href="#">Older</a>
     <a class="btn btn-outline-secondary disabled" href="#">Newer</a>
     </nav>
-
-</div><!-- /.blog-main -->
 
 @endsection
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,9 +2,22 @@
 
 @section('content')
 
-<div class="col-md-8 blog-main">
-    <h1>{{ $post->title }}</h1>
-    <p>{{ $post->body }}</p>
+<h1>{{ $post->title }}</h1>
+<p>{{ $post->body }}</p>
+<hr>
+<div class="comments">
+    <ul class="list-group">
+        @foreach ($post->comments as $comment)
+            <li class="list-group-item">
+                <strong>
+                    {{ $comment->created_at->diffForHumans() }} : &nbsp;
+                </strong>
+                {{ $comment->body }}
+            </li>
+            
+        @endforeach
+    </ul>
 </div>
+
     
 @endsection
