3d5194c539b5841f0ec10b5a565c7f277d4a76ba
cookiebear <cookiebear@cookiebearui-MacBook-Pro.local>
2018-09-17 15:14:16 +0900

    [Posts 저장] 1. form 추가 2. PostsController store 메서드 추가 3. form csrf_field 추가

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
+
 
 class PostsController extends Controller
 {
@@ -17,4 +19,35 @@ class PostsController extends Controller
     public function create() {
         return view('posts.create');
     }
+
+    public function store()
+    {
+        
+        // 1. request data를 이용한 새로운 Post 생성
+        // 2. 데이터 저장
+        
+        // 3. post 리스트 페이지 redirect
+
+        // dd(request()->all());
+
+        $post = new Post;
+
+        // 1. 데이터 저장 기본 방법
+        // $post->title = request('title');
+        // $post->body = request('body');
+
+        // $post->save();
+
+        // 2. 데이터 자장 방법
+        Post::create([
+            'title' => request('title'),
+            'body' => request('body')
+        ]);
+
+        // 3. 데이터 저장 방법
+        // Post::create(request()->all());
+
+        return redirect('/');
+
+    }  
 }
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,7 +1,20 @@
 @extends('layouts.master')
 
 @section('content')
-
-    <h1>Create a Post</h1>
+<div class="blog-post">
     
+    <form method="POST" action="/posts">
+        {{ csrf_field() }}
+        <div class="form-group">
+            <label for="title">제목</label>
+            <input type="text" class="form-control" id="title" name="title" placeholder="제목">
+        </div>
+        <div class="form-group">
+            <label for="body">내용</label>
+            <textarea class="form-control" name="body" id="body" cols="30" placeholder="내용"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">등록</button>
+    </form>
+    
+</div>
 @endsection
